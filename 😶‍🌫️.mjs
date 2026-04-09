b3648d61040d89b71a4d271e93501a012d72c1c4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-09 17:36:30 -0400

    0.0.3

--- 😶‍🌫️.mjs
+++ 😶‍🌫️.mjs
@@ -1,17 +1,17 @@
-import {emc as baseEmc} from './emc.mjs';
+import myJSON from './emc.json' with {type: 'json'};
 /** @import {EMC} from './types/mount-observer/types' */;
 /** @import {AllProps} from './types/be-decked-with/types' */
 
-console.log({emc});
+
 /**
  * @type {EMC<any, AllProps> }
  */
 const emc = {
     enhConfig: {
-        ...baseEmc.enhConfig,
+        ...myJSON.enhConfig,
         enhKey: '😶‍🌫️',
         withAttrs: {
-            ...baseEmc.withAttrs,
+            ...myJSON.enhConfig.withAttrs,
             base: '😶‍🌫️'
         }
     }
